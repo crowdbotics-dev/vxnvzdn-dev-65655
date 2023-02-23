19760a3fb300eabd99d4268fd08d3682bb39353b
Crowdbotics <engineering@crowdbotics.com>
2023-02-23 10:56:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/vxnvzdn_dev_65655/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vxnvzdn_dev_65655;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "vxnvzdn_dev_65655";
+  }
+}
